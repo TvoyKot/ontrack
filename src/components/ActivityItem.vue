1e2ae340a45bbc6a8170d99b6047d63b82f46f15
TvoyKot <a.lshe@yandex.by>
2024-09-05 11:11:20 +0300

    Continue_working_on_component_activities

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+import BaseButton from '../components/BaseButton.vue'
+import BaseSelect from '../components/BaseSelect.vue'
+import { TrashIcon } from '@heroicons/vue/24/solid'
+
+defineProps({
+  activity: {
+    type: String,
+    required: true
+  }
+})
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: '0:15'
+  },
+  {
+    value: 30,
+    label: '0:30'
+  },
+  {
+    value: 45,
+    label: '0:45'
+  },
+  {
+    value: 60,
+    label: '1:00'
+  },
+  {
+    value: 75,
+    label: '1:15'
+  },
+  {
+    value: 90,
+    label: '1:30'
+  },
+  {
+    value: 105,
+    label: '1:45'
+  },
+  {
+    value: 120,
+    label: '2:00'
+  }
+]
+
+const secondsToComplete = ref(null)
+</script>
+<template>
+  <li class="flex flex-col gap-2 p-4">
+    <div class="flex items-center gap-2">
+      <BaseButton>
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="truncate text-xl">{{ activity }}</span>
+    </div>
+    <div>
+      <BaseSelect class="font-mono" placeholder="h:mm" :selected="secondsToComplete" :options="periodSelectOptions" @select="secondsToComplete = $event"/>
+    </div>
+  </li>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,59 +1,11 @@
 <script setup>
-import BaseButton from '../components/BaseButton.vue'
-import BaseSelect from '../components/BaseSelect.vue'
-import { TrashIcon } from '@heroicons/vue/24/solid'
-
+import ActivityItem from '../components/ActivityItem.vue'
 const activities = ['Coding', 'Reading', 'Training']
-
-const periodSelectOptions = [
-    {
-        value: 15,
-        label: '0:15'
-    },
-    {
-        value: 30,
-        label: '0:30'
-    },
-    {
-        value: 45,
-        label: '0:45'
-    },
-    {
-        value: 60,
-        label: '1:00'
-    },
-    {
-        value: 75,
-        label: '1:15'
-    },
-    {
-        value: 90,
-        label: '1:30'
-    },
-    {
-        value: 105,
-        label: '1:45'
-    },
-    {
-        value: 120,
-        label: '2:00'
-    }
-]
 </script>
 <template>
   <div>
     <ul class="divide-y">
-      <li v-for="activity in activities" :key="activity" class="flex flex-col gap-2 p-4">
-        <div class="flex items-center gap-2">
-          <BaseButton>
-            <TrashIcon class="h-8" />
-          </BaseButton>
-          <span class="truncate text-xl">{{ activity }}</span>
-        </div>
-        <div>
-          <BaseSelect class="font-mono" placeholder="h:mm" :options="periodSelectOptions"/>
-        </div>
-      </li>
+      <ActivityItem v-for="activity in activities" :key="activity" :activity="activity"  />
     </ul>
   </div>
 </template>

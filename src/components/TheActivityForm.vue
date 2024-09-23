c687747a66e947978c4e71179d7342af7c6f5cfc
TvoyKot <a.lshe@yandex.by>
2024-09-23 19:12:59 +0300

    Added_component_TheActivitiesForm(refaktoring)

---
+++ src/components/TheActivityForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import BaseButton from './BaseButton.vue'
+import { PlusIcon } from '@heroicons/vue/24/solid'
+import { isActivityValid } from '../validators.js'
+
+let activity = ref('')
+
+const emit = defineEmits({
+  submit: isActivityValid,
+})
+function submit() {
+    emit('submit', activity.value)
+}
+
+</script>
+<template>
+  <form
+    @submit.prevent="submit"
+    class="sticky bottom-[77px] flex gap-2 border-t bg-white p-4"
+  >
+    <input
+      @input="activity = $event.target.value"
+      :value="activity"
+      type="text"
+      class="w-full rounded border px-4 text-xl"
+      placeholder="Activity name..."
+    />
+    <BaseButton>
+      <PlusIcon class="h-8" />
+    </BaseButton>
+  </form>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { ref } from 'vue'
 import ActivityItem from '../components/ActivityItem.vue'
-import BaseButton from '@/components/BaseButton.vue';
-import { PlusIcon } from '@heroicons/vue/24/solid'
+import TheActivityForm from '../components/TheActivityForm.vue'
 import { validateActivities, isActivityValid } from '../validators'
 
 defineProps({
@@ -17,9 +15,6 @@ const emit = defineEmits({
   createActivity: isActivityValid,
   deleteActivity: isActivityValid,
 })
-
-let newActivity = ref('')
-
 </script>
 <template>
   <div>
@@ -29,11 +24,6 @@ let newActivity = ref('')
       :key="activity" :activity="activity" 
       @delete="emit('deleteActivity', activity)"/>
     </ul>
-    <form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[77px] flex gap-2 border-t bg-white p-4">
-      <input @input="newActivity = $event.target.value" :value="newActivity" type="text" class="w-full rounded border px-4 text-xl" placeholder="Activity name...">
-      <BaseButton>
-        <PlusIcon class="h-8"/>
-      </BaseButton>
-    </form>
+    <TheActivityForm @submit="emit('createActivity', $event)"/>
   </div>
 </template>

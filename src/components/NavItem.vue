18349a83350ac2677f393680b44d866cd003aa23
TvoyKot <a.lshe@yandex.by>
2024-11-10 16:09:07 +0300

    Added refactoring component navItem

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,11 +1,22 @@
-<script>
-export default {
-  inheritAttrs: false
-}
+<script setup>
+import { computed } from 'vue'
+import { navigate, currentPage } from '../router.js'
+import { isPageValid } from '../validators.js'
+const props = defineProps({
+  page: {
+    required: true,
+    type: String,
+    validator: isPageValid
+  }
+})
+const classes = computed(() => [
+  'flex flex-col items-center p-2 capitalize text-sx',
+  { 'bg-gray-300 pointer-events-none': props.page === currentPage.value }
+])
 </script>
 <template>
   <li class="flex-1">
-    <a v-bind="$attrs" class="flex flex-col items-center p-2 capitalize text-sx">
+    <a :href="`#${page}`" :class="classes" @click="navigate(page)">
       <slot></slot>
     </a>
   </li>

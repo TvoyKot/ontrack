8c53ac3496c8eaff299b6db275e693eae5fcd8b8
TvoyKot <a.lshe@yandex.by>
2024-11-19 00:36:50 +0300

    Added component ProgressItem

--- src/activities.js
+++ src/activities.js
@@ -3,6 +3,11 @@ import { id } from './functions.js'
 import { SECONDS_IN_HOUR } from './constants.js'
 
 export const activities = ref(generateActivities())
+
+export const trackedActivities = computed(() =>
+  activities.value.filter(({ secondsToComplete }) => secondsToComplete)
+)
+
 export const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
 
 export function createActivity(activity) {
---
+++ src/components/ProgressItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(['index', 'activity'])
+
+const color = ['red', 'yellow', 'blue', 'green'][props.index]
+const progress = [10, 50, 70, 100][props.index]
+const timeProgress = ['03/30', '15/30', '21/30', '30/30'][props.index]
+</script>
+<template>
+  <li class="flex flex-col gap-1 p-4">
+    <div class="truncate text-xl">{{ activity.name }}</div>
+    <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
+      <div :class="`bg-${color}-500`" :style="`width: ${progress}%`"></div>
+    </div>
+    <div class="flex justify-between font-mono text-sm">
+      <span>{{ progress }}%</span><span>{{ timeProgress }}</span>
+    </div>
+  </li>
+</template>

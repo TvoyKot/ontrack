eb4b7edfd2adfe3dc3b6e9f09f5a1bfc29444046
TvoyKot <a.lshe@yandex.by>
2024-11-18 19:38:17 +0300

    Added  CSS-classes ICONS

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { ICONS } from '../icons.js'
-import { isIconValid } from '../validators.js'
+import { isIconValid, isNotEmptyString } from '../validators.js'
 defineProps({
   name: {
     required: true,
     type: String,
     validator: isIconValid,
+  },
+  classes: {
+    default: 'h-8',
+    type: String,
+    validator: isNotEmptyString
   }
 })
 </script>
 <template>
-  <component :is="ICONS[name]"></component>
+  <component :is="ICONS[name]" :class="classes"></component>
 </template>
